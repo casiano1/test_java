e687221e6f94b323293743fda15418580c9659f5
casiano1 <146191009+casiano1@users.noreply.github.com>
2023-12-22 14:51:46 +0100

    Add files via upload

---
+++ Java_learning/static/app.js
@@ -0,0 +1,15 @@
+const defaultResult = 0;
+let currentResult = 0;
+
+function add(num1, num2) {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+}
+
+
+
+addBtn.addEventListener('click', add);
+
+
+
+   
